81c3618d65c38c1973e1acf63057257059a819a0
rebecaborges <rebeca.felix@hotmail.com>
2019-03-25 15:07:41 -0300

    incluidos links das bibliotecas e frameworks

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyCK2okvnD6hv9H92LUd__YRLL0duvEWK9I",
+    authDomain: "redesocial-96ddc.firebaseapp.com",
+    databaseURL: "https://redesocial-96ddc.firebaseio.com",
+    projectId: "redesocial-96ddc",
+    storageBucket: "redesocial-96ddc.appspot.com",
+    messagingSenderId: "242338541780"
+  };
+  firebase.initializeApp(config);
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Rede Social
+
+## Objetivos
+
+## Definição do produto
+
+## Experiência de Usuário (UX Design)
+
+#### Personas:
+
+* Usuária:
+* Usuária:
+
+#### Protótipo:
+
+* Fidelidade:
+
+#### História de usuário:
