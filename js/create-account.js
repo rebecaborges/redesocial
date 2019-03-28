441160b6e2f0f5014be33da048b84c176f15b308
Rebeca <rebeca.felix@hotmail.com>
2019-03-28 13:27:28 -0300

    facebook authentication is working

--- js/create-account.js
+++ js/create-account.js
@@ -1,35 +1,58 @@
 $(document).ready(function () {
-  $("#signUpButton").click(function (event) {
-    const email = $("#signUpEmail").val()
-    const password = $("#signUpPassword").val()
-    event.preventDefault();
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
-      window.location = "form-perfil.html";
-    })
+  $("#signUpButton").click(signUp);
 
-      .catch(function (error) {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
+  $("#signUpButtonGoogle").click(signUpGoogle);
+
+  $("#signUpButtonFacebook").click(signUpFacebook);
+
+});
+
+function signUp (event) {
+  const email = $("#signUpEmail").val()
+  const password = $("#signUpPassword").val()
+  event.preventDefault();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
+    window.location = "form-perfil.html";
   })
 
-  $("#signUpButtonGoogle").click(function () {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-      const token = result.credential.accessToken;
-      const user = result.user;
-      window.location = "form-perfil.html";
+  .catch(function (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  
+}
 
-    }).catch(function (error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = error.credential;
-    });
-  })
+function signUpGoogle () {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    const token = result.credential.accessToken;
+    const user = result.user;
+    window.location = "form-perfil.html";
 
-})
+  }).catch(function (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.email;
+    const credential = error.credential;
+  });
+};
+
+function signUpFacebook (){
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+    window.location= "form-perfil.html";
+
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+  });
+}
 
 
 
