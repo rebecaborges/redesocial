acbc99f1f7cce1ffc6cd249de29dbe9105aeba4e
Iana-Neri22 <iananeri@outlook.com>
2019-04-07 12:54:09 -0300

    code cleaned

--- js/form-profile.js
+++ js/form-profile.js
@@ -1,44 +1,22 @@
-$(document).ready(function () {
-  $('#saveProfileBtn').click(function (event) {
-    const user = firebase.auth().currentUser
+$(document).ready( ()=> {
+  $('#saveProfileBtn').click(function(event) {
     event.preventDefault()
-
-    let name = $('#name').val()
+    const user = firebase.auth().currentUser
+    const name = $('#name').val()
   
   user.updateProfile({
     displayName: name
   })
-  console.log(user.displayName)
 
-    firebase.database().ref(`"profile/${user.uid}"`).update({
+    firebase.database().ref(`profile/${user.uid}`).update({
       surname: $('#surname').val(),
       profession: $('#profession').val(),
       city: $('#city').val(),
       state: $('#state').val(),
       birthDate: $('#birthDate').val(),
       userName: $('#userName').val()
-    }).then(function () {
+    }).then( ()=> {
       window.location = `timeline.html?id=${user.uid}`;
     })
   });
 })
-
-// function updateProfile(){
-//   let name = $('#name').val()
-//   const user = firebase.auth().currentUser
-//   user.updateProfile({
-//     displayName: name
-//   })
-//   console.log(user.displayName)
-// }
-
-
-
-
-
-
-
-
-
-
-
